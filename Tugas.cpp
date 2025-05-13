9f4b09939ba867c27e8ea947217d45647fe41296
FarabyMalau <farabymuzakki49@gmail.com>
2025-05-13 16:49:31 +0600

    Membuat class bangun datar dan persegi panjang

--- Tugas.cpp
+++ Tugas.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+//deklarasi class BangunDatar
+class BangunDatar;
+
+class PersegiPanjang{
+    public:
+        void inputData(BangunDatar &bd);
+        void outputData(BangunDatar &bd);
+};
